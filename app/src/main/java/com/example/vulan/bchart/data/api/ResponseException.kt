5c701af13d9ad7a3e3c258a171c6256ad8d4b7ad
Btnisme123 <vulan238@gmail.com>
2018-01-08 22:47:33 +0700

    response exception undone

--- app/src/main/java/com/example/vulan/bchart/data/api/ResponseException.kt
+++ app/src/main/java/com/example/vulan/bchart/data/api/ResponseException.kt
@@ -1,14 +1,95 @@
 package com.example.vulan.bchart.data.api
 
+import android.content.Context
+import com.example.vulan.bchart.R
+import retrofit2.Response
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
 /**
  * Created by FRAMGIA\nguyen.vu.lan on 1/8/18.
  */
-class ResponseException:RuntimeException() {
+class ResponseException : RuntimeException {
 
     private var type: Type? = null
+    private var errorResponse: ErrorResponse? = null
+    private var response: Response<Any>? = null
 
+    constructor(type: Type, errorResponse: ErrorResponse) {
+        this.type = type
+        this.errorResponse = errorResponse
+    }
 
-    enum class Type{
+    constructor(type: Type, response: Response<Any>) {
+        this.type = type
+        this.response = response
+    }
+
+    constructor(type: Type, throwable: Throwable) {
+        this.type = type
+        this.errorResponse = errorResponse
+    }
+
+    override fun getMessage(): String {
+        when (type) {
+            ResponseException.Type.SERVER -> if (errorResponse != null) {
+                return errorResponse!!.message
+            }
+        }
+
+        return super.message
+    }
+
+    fun getMessage(context: Context): String {
+        when (type) {
+            ResponseException.Type.SERVER -> {
+                return if (errorResponse != null) {
+                    errorResponse!!.message
+                } else ""
+            }
+            ResponseException.Type.NETWORK -> return getNetworkErrorMessage(context, cause)
+            ResponseException.Type.HTTP -> {
+                return if (response != null) {
+                    getHttpErrorMessage(context, response!!.code())
+                } else ""
+            }
+            ResponseException.Type.UNEXPECTED -> return super.message
+            else -> return ""
+        }
+    }
+
+    private fun getHttpErrorMessage(context: Context, httpCode: Int): String {
+        return ""
+    }
+    private fun getNetworkErrorMessage(context: Context, throwable: Throwable): String {
+        // TODO update later with Japanese
+        if (throwable is SocketTimeoutException) {
+            return context.getString(R.string.no_network_connection)
+        }
+
+        if (throwable is UnknownHostException) {
+            return context.getString(R.string.no_network_connection)
+        }
+
+        return if (throwable is IOException) {
+            context.getString(R.string.no_network_connection)
+        } else context.getString(R.string.no_network_connection)
+    }
+    companion object {
+        public fun toHTTPError(response: Response<Any>):ResponseException{
+            return ResponseException(Type.HTTP,response)
+        }
+
+        public fun toNetwork(throwable: Throwable):ResponseException{
+            return ResponseException(Type.NETWORK,throwable)
+        }
+
+        public fun toServerError(throwable: Throwable):ResponseException{
+            return ResponseException(Type.UNEXPECTED,throwable)
+        }
+    }
+    enum class Type {
         /**
          * An [IOException] occurred while communicating to the server.
          */
@@ -31,10 +112,12 @@ class ResponseException:RuntimeException() {
     fun getErrorCode(): String? {
         when (type) {
             ResponseException.Type.SERVER -> if (errorResponse != null) {
-                return errorResponse!!.getCode()
+                return errorResponse!!.code
             }
         }
 
         return null
     }
+
+
 }
